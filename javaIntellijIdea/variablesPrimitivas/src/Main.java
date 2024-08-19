86da3270561f5cbb6f30be15993a8a813775a7e5
tahayk3 <tahayk3@gmail.com>
2024-08-19 12:20:15 -0600

    variables primitivas, por referencia y de tipo var

---
+++ javaIntellijIdea/variablesPrimitivas/src/Main.java
@@ -0,0 +1,36 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //declarar variable
+        int variableInt;
+        //asignar variable
+        variableInt = 5;
+
+        //declaracion y asignacion de variable
+        double variableDouble = 5;
+
+        byte variableByte = 15;
+        char variableChar = 'A';
+        short variableShort = 15;
+        long variableLong = 25;
+        float variableFloat =1.0f;
+        boolean variableBoolean = true;
+
+        //Tipos de variable por referencia
+        String instrumento = "Guitarra";
+
+        System.out.println(instrumento);
+
+        //tipos de datos var
+        var edad = 20;
+        var nombre = "Juan";
+
+
+
+
+;
+
+
+    }
+}
