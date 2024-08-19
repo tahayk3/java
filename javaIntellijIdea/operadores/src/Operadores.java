354ea05259198085944068ce91dff50afdb08efa
tahayk3 <tahayk3@gmail.com>
2024-08-19 14:38:56 -0600

    operadores aritmeticos, logicos y relacionales en java

---
+++ javaIntellijIdea/operadores/src/Operadores.java
@@ -0,0 +1,36 @@
+public class Operadores {
+    public static void main(String[] args) {
+        /*
+        OPERADORES ARITMETICOS
+            + suma
+            - resta
+            * mutiplicacion
+            / division
+            % modulo
+        */
+        int num1 = 5;
+        int num2 = 5;
+        System.out.println(num1 + num2);
+
+        /*
+         OPERADORES RELACIONALES
+            < menor que
+            > mayor que
+            <= menor o igual
+            >= mayor o igual
+            == igual
+            != diferente
+        */
+        System.out.println(num1<num2);
+
+        /*
+         OPERADORES LOGICOS
+            && AND
+            || OR
+            ! negacion
+        */
+        System.out.println(!true);
+
+
+    }
+}
